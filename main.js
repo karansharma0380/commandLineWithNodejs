f0cb75382475d2e5c6e5b2b922cd579fe7209842
Karan <karansharma0380@gmail.com>
2021-11-14 12:56:07 +0530

    tree command developed

--- main.js
+++ main.js
@@ -34,6 +34,38 @@ switch(command){
 
 function treeFn(dirPath){
     console.log("Tree command implemented for ",dirPath);
+    if(dirPath==undefined){
+        console.log("Please enter the valid path for command");
+        return;
+    }
+    else{
+        let doespath = fs.existsSync(dirPath);
+        if(doespath){
+            treeHelper(dirPath,"");
+        }
+        else{
+            console.log("Please enter the valid path");
+            return;
+        }
+    }
+}
+
+function treeHelper(dirPath, indent){
+    // is  a file or folder 
+    let isFile = fs.lstatSync(dirPath).isFile();
+    if(isFile){
+        let fileName=path.basename(dirPath);
+        console.log(indent+"├──"+fileName);
+    }
+    else{
+        let dirName = path.basename(dirPath)
+        console.log(indent + "└──" + dirName);
+        let childrens = fs.readdirSync(dirPath);
+        for (let i = 0; i < childrens.length; i++) {
+            let childPath = path.join(dirPath, childrens[i]);
+            treeHelper(childPath, indent + "\t");
+        }
+    }
 }
 
 function organizeFn(dirPath){
